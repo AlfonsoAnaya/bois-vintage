2f8ed2096e2fd35a60da9559ce0aa7edc44e3a4b
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-05-18 18:33:54 +0200

    header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 function Header() {
     return (
         <header>
-            <h1>Bois</h1>
-            <p>vintage furniture</p>
+            <div className="grid-element">
+
+            </div>
+            <div className="grid-element">
+                <h1>Bois</h1>
+                <p className="subtitle">vintage furniture</p>
+            </div>
+            <div className="grid-element">
+
+            </div>
+
         </header>
     )
 }
