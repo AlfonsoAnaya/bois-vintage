acad2016dcd02c4e90f817784f71e5c6b415d6dc
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-05-19 12:34:50 +0200

    added svg icons to header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./sass/App.css";
 import Header from "./components/Header"
+
+import Footer from "./components/Footer"
 function App() {
   return (
     <div className="main-grid">
          <Header />
+         <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div>
+            <span>Icons by <a className="link" href="https://www.svgrepo.com/">svgrepo</a>.</span>
+        </div>
+    )
+}
+
+export default Footer
