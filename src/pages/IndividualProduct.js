5eb950c65524be3491eb48a12863a2607d6e45e9
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-05-26 18:23:33 +0200

    added gallery nagivation buttons

--- src/pages/IndividualProduct.js
+++ src/pages/IndividualProduct.js
@@ -1,17 +1,17 @@
-import {useState} from "react"
+import { useState } from "react"
 import { useParams } from "react-router-dom";
 import productsArr from "../data/products"
 
 
 function IndividualProduct() {
     let { id } = useParams();
-    let current = productsArr[id-1]
+    let current = productsArr[id - 1]
     let [currentPhoto, setCurrentPhoto] = useState(0)
 
     function nextPhoto() {
-        if(currentPhoto < 5) {
-            setCurrentPhoto(currentPhoto +1)
-            
+        if (currentPhoto < 5) {
+            setCurrentPhoto(currentPhoto + 1)
+
         } else {
             setCurrentPhoto(0)
         }
@@ -19,21 +19,37 @@ function IndividualProduct() {
     }
 
     function prevPhoto() {
-        if(currentPhoto > 0) {
-            setCurrentPhoto(currentPhoto -1)
-        
+        if (currentPhoto > 0) {
+            setCurrentPhoto(currentPhoto - 1)
+
         } else {
             setCurrentPhoto(5)
         }
+
+    }
+
+    function selectphoto(imgNum) {
+        setCurrentPhoto(imgNum)
         console.log(currentPhoto)
     }
 
     return (
         <div className="product-container">
             <div className="gallery">
-                <img src={current.gallery[currentPhoto]} alt={current.description}/>
-                <button className="btn btn-next" onClick={nextPhoto} > → </button>
-                <button className="btn btn-prev" onClick={prevPhoto}> ← </button>
+                <div className="gallery-imgs">
+                    <img src={current.gallery[currentPhoto]} alt={current.description} />
+                    <button className="btn btn-next" onClick={nextPhoto} > → </button>
+                    <button className="btn btn-prev" onClick={prevPhoto}> ← </button>
+                </div>
+
+                <div className="gallery-nav-container">
+                    <div className={currentPhoto===0 ? "active gallery-nav-item" : "gallery-nav-item"} onClick={() => selectphoto(0)}></div>
+                    <div className={currentPhoto===1 ? "active gallery-nav-item" : "gallery-nav-item"} onClick={() => selectphoto(1)}></div>
+                    <div className={currentPhoto===2 ? "active gallery-nav-item" : "gallery-nav-item"} onClick={() => selectphoto(2)}></div>
+                    <div className={currentPhoto===3 ? "active gallery-nav-item" : "gallery-nav-item"} onClick={() => selectphoto(3)}></div>
+                    <div className={currentPhoto===4 ? "active gallery-nav-item" : "gallery-nav-item"} onClick={() => selectphoto(4)}></div>
+                    <div className={currentPhoto===5 ? "active gallery-nav-item" : "gallery-nav-item"} onClick={() => selectphoto(5)}></div>
+                </div>
             </div>
             <div className="product-details">
                 <h2>{current.title}</h2>
